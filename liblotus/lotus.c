f189300079ad16fd52e4765ecf2382e829d14f92
waozixyz <waozi@proton.me>
2026-05-21 22:01:57 -0300

    stared liblotus

---
+++ liblotus/lotus.c
@@ -0,0 +1,23 @@
+#include "lotus.h"
+
+void
+inccount(char v[CountSize])
+{
+	if(v[2] < '9'){
+		v[2]++;
+		return;
+	}
+	v[2] = '0';
+
+	if(v[1] < '9'){
+		v[1]++;
+		return;
+	}
+	v[1] = '0';
+
+	if(v[0] < '9'){
+		v[0]++;
+		return;
+	}
+	v[0] = '0';
+}
