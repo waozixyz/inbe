99567e68c13a4d5de4afa74910141cc20d28d85b
waozixyz <waotzi@proton.me>
2026-05-29 11:19:29 -0300

    add image fallback

--- src/ui.h
+++ src/ui.h
@@ -11,6 +11,24 @@ typedef enum {
     UI_ICON_SIZE_LARGE
 } UIIconSize;
 
+typedef enum {
+    UI_ICON_TYPE_NONE,
+    UI_ICON_TYPE_GEAR,
+    UI_ICON_TYPE_X,
+    UI_ICON_TYPE_MANUAL,
+    UI_ICON_TYPE_RETURN,
+    UI_ICON_TYPE_BACKWARD,
+    UI_ICON_TYPE_FORWARD,
+    UI_ICON_TYPE_PLAY,
+    UI_ICON_TYPE_PAUSE,
+    UI_ICON_TYPE_STAT,
+    UI_ICON_TYPE_TELEGRAM,
+    UI_ICON_TYPE_GLOBE,
+    UI_ICON_TYPE_STRIPE,
+    UI_ICON_TYPE_MONERO,
+    UI_ICON_TYPE_HOME
+} UIIconType;
+
 void ui_init(int width, int height, float dpi);
 void ui_set_colors(Color text, Color bg, Color circle, Color button, Color button_hover, Color icon);
 int ui_px(int px);
@@ -22,10 +40,10 @@ void ui_draw_bevel(int x, int y, int w, int h, Color light, Color dark);
 void ui_draw_text_lines(const char **lines, int count, int x, int *y, int font, int line_h);
 int ui_icon_btn_size(UIIconSize size);
 int ui_icon_btn_padding(UIIconSize size);
-int ui_draw_icon_btn(InbeApp *app, int x, int y, UIIconSize size, Texture2D icon, int *hover);
-int ui_draw_icon_btn_padded(InbeApp *app, int x, int y, int size, Texture2D icon, int *hover);
+int ui_draw_icon_btn(InbeApp *app, int x, int y, UIIconSize size, Texture2D icon, UIIconType icon_type, int *hover);
+int ui_draw_icon_btn_padded(InbeApp *app, int x, int y, int size, Texture2D icon, UIIconType icon_type, int *hover);
 int ui_draw_text_btn(InbeApp *app, int x, int y, const char *label, int *hover);
-void ui_draw_icon_link(InbeApp *app, int x, int y, int icon_size, Texture2D icon, const char *url);
+void ui_draw_icon_link(InbeApp *app, int x, int y, int icon_size, Texture2D icon, UIIconType icon_type, const char *url);
 int ui_draw_slider(InbeApp *app, int id, int x, int y, int w, const char *label, int min, int max, int *value, const char *suffix);
 int ui_draw_toggle_switch(InbeApp *app, int x, int y, int w, int h, int *value);
 int ui_draw_checkbox_toggle(InbeApp *app, int x, int y, const char *label, int *value);
@@ -35,6 +53,7 @@ int ui_dropdown_captures_click(Vector2 point);
 typedef struct UITab {
     const char *label;
     Texture2D icon;
+    UIIconType icon_type;
     void (*on_click)(void *user_data);
     void *user_data;
 } UITab;
@@ -45,8 +64,8 @@ typedef struct UITabBar {
 } UITabBar;
 
 int ui_nav_button_width(const char *label, int icon_size, int show_label, int font);
-int ui_draw_nav_button(InbeApp *app, int x, int y, int icon_size, Texture2D icon, const char *label, int show_label, int *hover);
-int ui_draw_nav_button_expand(InbeApp *app, int x, int y, int icon_size, int w, Texture2D icon, const char *label, int show_label, int *hover);
+int ui_draw_nav_button(InbeApp *app, int x, int y, int icon_size, Texture2D icon, UIIconType icon_type, const char *label, int show_label, int *hover);
+int ui_draw_nav_button_expand(InbeApp *app, int x, int y, int icon_size, int w, Texture2D icon, UIIconType icon_type, const char *label, int show_label, int *hover);
 void ui_draw_tab_bar(UITab *tabs, int count, InbeApp *app);
 void ui_draw_tutorial_image_placeholder(const char *label, int x, int y, int w, int h);
 void ui_draw_tutorial_image(Texture2D texture, const char *fallback, int x, int y, int w, int h);
