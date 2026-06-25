37856cef756c24954b52baf99d41aa52f5364618
waozixyz <waozi@proton.me>
2026-06-24 21:26:19 -0300

    Prevent inactive guide input blocking

--- src/app/app.c
+++ src/app/app.c
@@ -1954,8 +1954,7 @@ finish_frame:
         ui_set_input_blocked(0);
     practice_screen_draw_first_run_guide(app);
     habits_screen_draw_first_run_guide(app);
-    if(!guide_active)
-        ui_set_input_blocked(0);
+    ui_set_input_blocked(0);
     draw_global_modal(app);
     app_flush_deferred_settings(app);
     app_observe_direct_screen_change(app, frame_screen);
